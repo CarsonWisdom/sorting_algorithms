60bd4cbe4109611a9c00d0193f586a1106350906
CarsonWisdom <amoswwmayoka@gmail.com>
2023-11-26 10:12:54 -0800

    radixsort

--- deck.h
+++ deck.h
@@ -1,19 +1,13 @@
 #ifndef DECK_H
 #define DECK_H
 
-/**
- * enum kind_e - card kind
- * @SPADE: spade kind
- * @HEART: heart kind
- * @CLUB: club kind
- * @DIAMOND: diamond kind
- */
+
 typedef enum kind_e
 {
-	SPADE = 0,
-	HEART,
-	CLUB,
-	DIAMOND
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
 } kind_t;
 
 /**
@@ -25,23 +19,26 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-	const char *value;
-	const kind_t kind;
+    const char *value;
+    const kind_t kind;
 } card_t;
 
 /**
  * struct deck_node_s - Deck of card
  *
- * @card: Pointer to card of the node
- * @prev: Pointer to previous node of the list
- * @next: Pointer t  next node of the list
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
  */
 typedef struct deck_node_s
 {
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
 } deck_node_t;
 
+
 void sort_deck(deck_node_t **deck);
-#endif
+
+
+#endif /* DECK_H */
